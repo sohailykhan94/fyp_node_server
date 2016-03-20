acf8577b47271affd0b44986cb7de27f903d8586
Sohail Yar Khan <sohailykhan94@gmail.com>
2016-03-20 19:53:09 +0800

    Pyth Input

---
+++ routes/python_shell.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+var fs = require('fs');
+var PythonShell = require('python-shell');
+var options = {
+  mode: 'text',
+  scriptPath: '/home/sohailyarkhan/node-server/fyp_node_server/'
+};
+var pyshell = new PythonShell('single_predict_input.py',options);
+
+router.use(function(req, res, next){
+  console.log('Prediction Single Input Python API');
+  next();
+})
+
+router.get('/', function(req, res, next) {
+
+  if(req.query.day && req.query.month && req.query.year && req.query.hours && req.query.minutes && req.query.src && req.query.des){
+    var day = parseInt(req.query.day);
+    var month = parseInt(req.query.month);
+    var year = parseInt(req.query.year);
+    var hours = parseInt(req.query.hours);
+    var minutes = parseInt(req.query.minutes);
+    var src = parseInt(req.query.src);
+    var des = parseInt(req.query.des);
+    var tempJSON = JSON.parse(fs.readFileSync('./speedMapSrcDes.json', 'utf8'));
+    for(var i= 0;i<tempJSON.length-1;i++){
+      if(tempJSON[i].src == src && tempJSON[i].des == des){
+        var src_lat = tempJSON[i].src_lat;
+        var src_long = tempJSON[i].src_long;
+        var des_lat = tempJSON[i].des_lat;
+        var des_long = tempJSON[i].des_long;
+      }
+    }
+    var queryString = String(req.query.year) + " " + String(req.query.month) + " " + String(req.query.day) + " " + String(req.query.hours) + " " + String(req.query.minutes) + " " + String(req.query.src) + " " + String(req.query.des);
+    console.log(queryString);
+    // executes `pwd`
+    pyshell.send(queryString);
+    pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log(message); 
+    if(message){
+      message = message.replace('[\'','');
+      message = message.replace('\']','');
+      message = message.replace(/(?:\r\n|\r|\n)/g,'');
+      var label = message;
+        var result = {
+          src_lat: src_lat,
+          src_long: src_long,
+          des_lat: des_lat,
+          des_long: des_long,
+          road_saturation_level: label
+        }
+      res.status(200);
+      res.json({status: 'success', nodes: result});
+    }else{
+      res.status(404);
+      res.json({status: 'error', data: 'error'});
+    }
+    });
+  }else{
+    res.status(404);
+    res.json({status: 'error', data: 'error'});
+  }
+});
+
+//2,11,2014,19,0,4651,4631, 30
+
+module.exports = router;
---
+++ single_predict_input.py
@@ -0,0 +1,58 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+from datetime import timedelta
+from datetime import datetime
+from datetime import date
+from datetime import time
+import dateutil.parser
+import sys
+import random
+import numpy as np
+import cPickle
+np.random.seed(21)
+
+from sklearn import linear_model, cross_validation, metrics, svm
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+
+import pandas as pd
+
+endSignal = False
+
+def predict(str):
+    str_array = str.split()
+    if len(str_array) != 7:
+        return "Incorrect Input"
+    options = {1 : 'jan',
+               2 : 'feb',
+               3 : 'mar',
+               4 : 'apr',
+               5 : 'may',
+               6 : 'jun',
+               7 : 'jul',
+               8 : 'aug',
+               9 : 'sep',
+               10 : 'oct',
+               11 : 'nov',
+               12 : 'dec'
+    }
+    month = options[int(str_array[1])]
+    d = date(int(str_array[0]), int(str_array[1]), int(str_array[2]))
+    t = time(int(str_array[3]), int(str_array[4]))
+    dateObj = datetime.combine(d, t)
+    dateObj = dateObj - timedelta(hours = 8)
+    importString = '/home/sohailyarkhan/pythonLearningModels/'+month+'/model.pkl'
+    with open(importString, 'rb') as fid:
+        SGD = cPickle.load(fid)
+    final_data = pd.DataFrame(np.array([[int(dateObj.year), int(dateObj.month), int(dateObj.day), int(dateObj.hour), int(dateObj.minute), int(str_array[5]), int(str_array[6])]]), columns=['year','month','day','hour','minute','src','des'])
+    pred_new = SGD.predict(final_data)
+    return pred_new
+
+while endSignal != True:
+    x = raw_input("Enter variable ")
+    if x=='endSignal':
+        endSignal = True
+    else:
+        print predict(x)
