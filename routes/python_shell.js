a07ecd16a232f85142b5943d0be22775b5a722f9
Sohail Yar Khan <sohailykhan94@gmail.com>
2016-03-20 22:02:55 +0800

    test

--- routes/python_shell.js
+++ routes/python_shell.js
@@ -62,7 +62,10 @@ router.get('/', function(req, res, next) {
         res.json({status: 'error', data: 'error'});
       }
     });
-
+    pyshell.end(function (err) {
+      if (err) throw err;
+      console.log('finished');
+    });
   }else{
     res.status(404);
     res.json({status: 'error', data: 'error'});
